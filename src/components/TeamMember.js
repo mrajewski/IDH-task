d4ce7a2303859999b4b126e5458680cc2a5862e0
mrajewski <rajewski.ma@gmail.com>
2019-10-22 20:16:04 +0200

    started team member component

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -3,13 +3,20 @@ import React from 'react';
 function TeamMember(props) {
 
     return (
-        <div className="team-member">
+        <div key={props.id} className="team-member">
             <div className="member-mail">
             </div>
-            <img src={props.img} alt="" className="member-img"/>
+            <div className="member-avatar">
+                <span className="member-img-over"/>
+                <img src={props.img} alt="" className="member-img"/>
+            </div>
             <h3 className="member-name">{props.name}</h3>
             <p className="member-position">{props.position}</p>
             <p className="member-location">{props.location}</p>
+            <a href={props.mail} className="member-mail">
+
+
+            </a>
 
         </div>
     )
