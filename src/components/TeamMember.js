912edaaea6103ea13adb09a2a6518f7cb02265c2
mrajewski <rajewski.ma@gmail.com>
2019-10-23 16:32:09 +0200

    landing page styles

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,23 +1,20 @@
 import React from 'react';
+import mail from '../assets/img/Mail closed.svg'
 
 function TeamMember(props) {
 
     return (
         <div key={props.id} className="team-member">
-            <div className="member-mail">
-            </div>
             <div className="member-avatar">
                 <span className="member-img-over"/>
                 <img src={props.img} alt="" className="member-img"/>
+                <a href={`mailto:${props.mail}`} className="member-mail">
+                    <img className='member-envelope' src={mail} alt=""/>
+                </a>
             </div>
             <h3 className="member-name">{props.name}</h3>
             <p className="member-position">{props.position}</p>
             <p className="member-location">{props.location}</p>
-            <a href={props.mail} className="member-mail">
-
-
-            </a>
-
         </div>
     )
 
--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import teamData from "./teamData";
 import "./styles.scss";
-
 import TeamMember from "./components/TeamMember";
+
 
 function App() {
 
     return (
         <>
-            <TeamMember img={teamData[0].img}
-                        name={teamData[0].name}
-                        position={teamData[0].position}
-                        location={teamData[0].location}
-                        mail={teamData[0].mail}
-                        id={teamData[0].id}
-            />
+            <h1 className="page-title">Meet our team</h1>
+            <section className="team-members-container">
+            {teamData.map(el=>{
+                return (
+                    <TeamMember img={el.img}
+                                name={el.name}
+                                position={el.position}
+                                location={el.location}
+                                mail={el.mail}
+                                id={el.id}
+                                key={el.id}
+                    />
+                )
+            })}
+            </section>
+
         </>
     )
 }
