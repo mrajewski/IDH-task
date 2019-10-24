ce241d22f3abb329b082312f8924423966510737
mrajewski <rajewski.ma@gmail.com>
2019-10-24 17:23:56 +0200

    Updates

---
+++ precache-manifest.f25f0c84e8884b7435ff5546f7f3f272.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4479d3c5e2a2a4b30fb81b2470edeb66",
+    "url": "/IDH-task/index.html"
+  },
+  {
+    "revision": "4a692e58a65f8f24799b",
+    "url": "/IDH-task/static/css/main.d27fbcc0.chunk.css"
+  },
+  {
+    "revision": "7b8261d90c03bc6048fa",
+    "url": "/IDH-task/static/js/2.8c9a4450.chunk.js"
+  },
+  {
+    "revision": "4a692e58a65f8f24799b",
+    "url": "/IDH-task/static/js/main.e006c431.chunk.js"
+  },
+  {
+    "revision": "f7ad84ae3caee6f7632a",
+    "url": "/IDH-task/static/js/runtime~main.7e1b0e1e.js"
+  },
+  {
+    "revision": "d3b41df5202c4c41993f32a6d896a908",
+    "url": "/IDH-task/static/media/Mail closed.d3b41df5.svg"
+  },
+  {
+    "revision": "67200325e54fbb92c9669f0f5b0bc533",
+    "url": "/IDH-task/static/media/Man1.67200325.png"
+  },
+  {
+    "revision": "4e024da8e52c863dad015bd481381b5a",
+    "url": "/IDH-task/static/media/Man2.4e024da8.png"
+  },
+  {
+    "revision": "d9958707a1f22cddf3ab7bb93741dbf7",
+    "url": "/IDH-task/static/media/Woman1.d9958707.png"
+  },
+  {
+    "revision": "d7535b9e62f93069aa9bd0512fb07022",
+    "url": "/IDH-task/static/media/Woman2.d7535b9e.png"
+  },
+  {
+    "revision": "02177fa9e67c03f057a64b10fafc9c12",
+    "url": "/IDH-task/static/media/Woman3.02177fa9.png"
+  },
+  {
+    "revision": "86377fca9a2188d4d1eaafa1576cf3e1",
+    "url": "/IDH-task/static/media/bg.86377fca.png"
+  },
+  {
+    "revision": "a9d531efe3e11057862c414db4f55d2d",
+    "url": "/IDH-task/static/media/slide.a9d531ef.png"
+  }
+]);
